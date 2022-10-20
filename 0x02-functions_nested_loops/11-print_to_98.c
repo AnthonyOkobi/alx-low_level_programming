538e414ae889b702a4506e63bcd605ea606bc3b8
AnthonyOkobi <okobitony@gmail.com>
2022-10-20 01:15:00 +0000

    Learning C program fuctions nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,12 +7,23 @@
  */
 void print_to_98(int n)
 {
-if (n < 98)
+if (n > 98)
+{
+while (n >= 98)
+{
+printf("%d", n);
+if(n != 98)
+{
+printf("' ");
+}
+n--;
+}
+}
+else
 {
 while (n <= 98)
 {
 printf("%d", n);
-
 if (n != 98)
 {
 printf(", ");
@@ -20,18 +31,6 @@ printf(", ");
 n++;
 }
 }
-
-else if (n > 98)
-{
-while (n >= 98)
-{
-printf(", ");
-n--;
-}
-}
-else
-{
-printf("98");
-}
 printf("\n");
 }
+
