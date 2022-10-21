e10335ac700c58551bf50a0bd2cb91c2b634a01c
AnthonyOkobi <okobitony@gmail.com>
2022-10-21 19:42:59 +0000

    program that prints the numbers from 1 to 100, followed by a new line

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,7 +15,7 @@ for (a = 1; a <= 100; a++)
 if ((a % 3) == 0 && (a % 5) != 0)
 printf("Fizz ");
 else if (a == 100)
-printf("Buzz ");
+printf("Buzz");
 else if ((a % 5) == 0 && (a % 3) != 0)
 printf("Buzz ");
 else if ((a % 3) == 0 && (a % 3) == 0)
