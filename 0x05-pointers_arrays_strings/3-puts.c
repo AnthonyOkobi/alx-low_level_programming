41aa97c63662d523563f5a160a18f3d239c6eb89
AnthonyOkobi <okobitony@gmail.com>
2022-10-25 14:33:46 +0000

    function that prints a string, followed by a new line, to stdout

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,11 +3,10 @@
 /**
 * _puts - prints a string with new line
 * @str: the string to print
-* Return: string 
+* Return: string
 */
 void _puts(char *str)
 {
-  
 int i;
 
 for (i = 0; *(str + i) != '\0'; i++)
