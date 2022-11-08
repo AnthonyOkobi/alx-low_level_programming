d83c776292adb10d5016a54d5823d5b15e720b6b
AnthonyOkobi <okobitony@gmail.com>
2022-11-08 16:56:55 +0000

    a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,9 +19,9 @@ if (str == NULL)
 return (NULL);
 while (str[i])
 i += 1;
-p = malloc(i * sizeof(char) + 1);
+p = malloc(i *sizeof(char) + 1);
 
-if (p == NULL) 
+if (p == NULL)
 return (NULL);
 while (*str)
 {
