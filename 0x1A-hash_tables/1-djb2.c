c61c6109e2ab99d7bcd1bb93f3f6c69209a8881c
AnthonyOkobi <okobitony@gmail.com>
2023-02-03 07:11:44 -0800

    Hash table

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -4,18 +4,18 @@
  *
  * @str: const unsigned char ptr to string to be hashed
  *
- * Rturn: a hash
+ * Return: a hash
  */
 
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+unsigned long int hash;
+int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+hash = 5381;
+while ((c = *str++))
+{
+hash = ((hash << 5) + hash)+c; /* hash * 33 + c */
+}
+return (hash);
 }
